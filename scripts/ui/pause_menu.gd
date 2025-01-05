ad2c7bce98d0a00ef18d476c6e70844bcbd10407
Murilo <lilo.marino@hotmail.com>
2025-01-05 17:41:17 -0300

    fix: :wrench: Corrigido pause também pausando menu principal ao voltar

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -1,11 +1,5 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 func _on_resume_pressed() -> void:
 	get_parent()._resume_game()	
 
@@ -15,6 +9,7 @@ func _on_options_pressed() -> void:
 
 
 func _on_menu_exit_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
 
 
