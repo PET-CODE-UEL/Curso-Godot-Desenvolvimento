58a8c1f22c559e7b7c071c54a59743ade6dc1b7b
LiloMarino <lilo.marino@hotmail.com>
2025-01-28 11:47:19 -0300

    fix: :wrench: Corrigido erros no inventário

--- scripts/ui/inventory/inventory.gd
+++ scripts/ui/inventory/inventory.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@export var slot_scene: PackedScene
 @onready var hotbar = $Margin/Panel/Margin/VBox/Hotbar
 @onready var inventory_grid = $Margin/Panel/Margin/VBox/InventoryGrid
 
@@ -8,14 +9,18 @@ func _ready():
     setup_inventory()
 
 func setup_hotbar():
+    clear_children(hotbar)  # Limpa os slots existentes
     for i in range(9):
-        var slot = hotbar.get_child(i)
-        slot.set_item(InventoryManager.get_hotbar_item(i))
+        var slot = slot_scene.instantiate()  # Instancia o slot
+        hotbar.add_child(slot)  # Adiciona o slot à hotbar
+        slot.set_item(InventoryManager.get_hotbar_item(i))  # Define o item da hotbar
 
 func setup_inventory():
+    clear_children(inventory_grid)  # Limpa os slots existentes
     for i in range(27):
-        var slot = inventory_grid.get_child(i)
-        slot.set_item(InventoryManager.get_inventory_item(i))
+        var slot = slot_scene.instantiate()  # Instancia o slot
+        inventory_grid.add_child(slot)  # Adiciona o slot ao inventário
+        slot.set_item(InventoryManager.get_inventory_item(i))  # Define o item do inventário
 
 func update_inventory():
     for i in range(9):
@@ -25,3 +30,7 @@ func update_inventory():
     for i in range(27):
         var slot = inventory_grid.get_child(i)
         InventoryManager.set_inventory_item(i, slot.item)
+
+func clear_children(node: Node):
+    for child in node.get_children():
+        child.queue_free()
